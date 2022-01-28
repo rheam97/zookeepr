407525199f9766759f00ee4eac1da45d9a845957
Rhea <rheam97@gmail.com>
2022-01-27 19:05:25 -0500

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { query } = require('express')
 const e = require('express')
 const express = require('express')
+const PORT = process.env.PORT || 3001 
 const app = express()
 const {animals} = require('./data/animals.json')
 
@@ -41,6 +42,6 @@ app.get('/api/animals', (req,res)=> {
 
     res.json(results)
 })
-app.listen(3001, ()=> {
-    console.log('API server now on port 3001!')
+app.listen(PORT, ()=> {
+    console.log(`API server now on port ${PORT}!`)
 })

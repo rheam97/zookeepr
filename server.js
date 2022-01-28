72ea3d837e17841281bd79a5257c01511b25211b
Rhea <rheam97@gmail.com>
2022-01-27 19:19:35 -0500

    query and param routes and requests est. deployed to heroku

--- server.js
+++ server.js
@@ -34,6 +34,11 @@ function filterbyQuery(query, animalsArray){
     }
     return filteredResults
 }
+
+function findById(id, animalsArray) {
+const result = animalsArray.filter(animal=> animal.id===id)[0]
+return result
+}
 app.get('/api/animals', (req,res)=> {
     let results = animals
   if (req.query){
@@ -41,6 +46,16 @@ app.get('/api/animals', (req,res)=> {
   }
 
     res.json(results)
+})
+app.get('/api/animals/:id', (req, res)=> {
+    const result = findById(req.params.id, animals)
+    if (result) {
+        res.json(result)
+    }
+    else{
+        res.sendStatus(404)
+    }
+  
 })
 app.listen(PORT, ()=> {
     console.log(`API server now on port ${PORT}!`)

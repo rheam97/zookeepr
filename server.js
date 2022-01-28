16d4c006209a75c023b6e3c60e890cb6fdc71768
Rhea <rheam97@gmail.com>
2022-01-27 19:01:07 -0500

    add heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+const { query } = require('express')
+const e = require('express')
+const express = require('express')
+const app = express()
+const {animals} = require('./data/animals.json')
+
+function filterbyQuery(query, animalsArray){
+    let personalityTraitsArray = []
+    let filteredResults = animalsArray
+    if(query.personalityTraits){
+        if( typeof query.personalityTraits=== 'string'){
+            personalityTraitsArray=[query.personalityTraits]
+        }
+        else{
+            personalityTraitsArray=query.personalityTraits
+        }
+        personalityTraitsArray.forEach(trait=> {
+            filteredResults=filteredResults.filter(
+                animal=> animal.personalityTraits.indexOf(trait)!== -1
+            )
+        })
+    }
+ 
+
+    if(query.diet){
+        filteredResults= filteredResults.filter(animal => animal.diet===query.diet)
+    }
+    if(query.species){
+        filteredResults= filteredResults.filter(animal => animal.species===query.species)
+    }
+    if(query.name){
+        filteredResults=filteredResults.filter(animal=> animal.name===query.name)
+    }
+    return filteredResults
+}
+app.get('/api/animals', (req,res)=> {
+    let results = animals
+  if (req.query){
+    results=filterbyQuery(req.query, results)
+  }
+
+    res.json(results)
+})
+app.listen(3001, ()=> {
+    console.log('API server now on port 3001!')
+})

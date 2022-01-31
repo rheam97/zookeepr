1522943f45f6bf5b92a172481a4176c685f1ef02
Rhea <rheam97@gmail.com>
2022-01-31 09:04:05 -0500

    merged front end with server routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static('public'))
 const {animals} = require('./data/animals.json')
 
 function filterbyQuery(query, animalsArray){
@@ -96,6 +97,21 @@ app.post('/api/animals/', (req, res)=> {
     }
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 app.listen(PORT, ()=> {
     console.log(`API server now on port ${PORT}!`)
 })
